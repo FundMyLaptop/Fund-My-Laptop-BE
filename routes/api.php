d022060f5b208c5106961735b84dca12fbe67456
trustidkid <yemi.bili07@gmail.com>
2020-06-28 10:46:12 +0100

    Fixes #7 resolve merge conflict

--- routes/api.php
+++ routes/api.php
@@ -26,26 +26,14 @@ Route::group(['namespace' => 'Api', 'middleware' => 'auth:api',  'prefix' => 'v1
 	/*Route::get('/user', function (Request $request) {
     	return $request->user();
 	});*/
-<<<<<<< HEAD
 	Route::get('recommendations','RecommendationController@index');
     ///all other routes should be defined under this line using the format of line 25 (above)
 
     Route::post('requests','RequestController@store');
-=======
-    Route::get('recommendations','RecommendationController@index');
     Route::get('user-details', 'UserController@getUserDetails');
 	///all other routes should be defined under this line using the format of line 25 (above)
->>>>>>> 211e8e921c9b78891ffe19a52927d07bb998b93b
 });
 
-<<<<<<< HEAD
 Route::fallback(function(){
     return response()->json(['message' => 'Not Found'], 404);
 })->name('api.fallback.404');
-
-=======
-Route::post('v1/request', 'RequestsController@insert');
-
-Route::post('v1/order', 'RequestsController@create');
->>>>>>> Fixes #7
-
