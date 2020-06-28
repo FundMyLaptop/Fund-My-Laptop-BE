d096c25bda1504f8f3af4dcb2679a75969375d12
kelrob <test>
2020-06-28 10:31:19 +0100

    Stashed Commit

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::group(['namespace' => 'Api', 'middleware' => 'auth:api',  'prefix' => 'v1
     	return $request->user();
 	});*/
     Route::get('recommendations','RecommendationController@index');
-    Route::get('user-details', 'UserController@getUserDetails');
+    Route::get('my-profile', 'UserController@getMyProfile');
 	///all other routes should be defined under this line using the format of line 25 (above)
 });
 
