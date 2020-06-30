d0c3e10b6dce3339c1ba87646fc678c08cac809f
kelrob <test>
2020-06-30 11:05:10 +0100

    Last working code

--- routes/api.php
+++ routes/api.php
@@ -32,25 +32,25 @@ Route::group(['namespace' => 'Api', 'middleware' => 'auth:api',  'prefix' => 'v1
 
     ///all other routes should be defined under this line using the format of line 25 (above)
     Route::get('verified-users', 'VerificationController@index');
-	  Route::get('recommendations', 'RecommendationController@index');
-	  Route::get('my-profile', 'UserController@getMyProfile');
+	Route::get('recommendations', 'RecommendationController@index');
+	Route::get('my-profile', 'UserController@getMyProfile');
     Route::get('requests', 'RequestController@index');
     Route::get('requests/{id}', 'RequestController@show');
     Route::post('bank-accounts', 'BankAccountController@create');
     Route::get('completed-requests', 'AdminController@index');
     Route::post('transaction/store','TransactionController@store');
-	  Route::post('transaction/update/{id}','TransactionController@update');
-	  Route::post('bank-accounts', 'BankAccountController@create');
-	  Route::get('completed-requests', 'AdminController@index');
+	Route::post('transaction/update/{id}','TransactionController@update');
+	Route::post('bank-accounts', 'BankAccountController@create');
+	Route::get('completed-requests', 'AdminController@index');
     Route::delete('users/delete/{id}','AdminController@destroy');
     Route::get('transaction/funder/{id}', 'TransactionController@getFunderHistory');
-    Route::post('save-verification-file','VerificationController@store');
 
 
 
     // Commented out by Eromosele
     //Route::post('transaction/store', 'TransactionController@store');
     //Route::post('transaction/update/{id}','TransactionController@update');
+
 });
 Route::post('/password/email', 'Api\ForgotPasswordController@sendResetLinkEmail'); //For sending email link
 Route::post('/password/reset', 'Api\ResetPasswordController@reset');  //For resetting the password
