f5eddecd3252a7951d5ce31091452bf274ac2b33
trustidkid <yemi.bili07@gmail.com>
2020-06-30 22:30:48 +0100

    Fixes #7 deleted unneeded files

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -37,10 +37,10 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            //'url' => env('DATABASE_URL'),
+            'url' => env('DATABASE_URL'),
             'database' => env('DB_DATABASE'),//,database_path('database.sqlite')),
-            //'prefix' => '',
-            //'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
         'mysql' => [
