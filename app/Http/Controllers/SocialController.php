d1d4bec3ae77e580caa0473749a885d35a56ea6e
Onifs10 <onifadebolu64@gmail.com>
2020-06-30 19:10:33 +0100

    twitterlogin

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -23,7 +23,7 @@ class SocialController extends Controller
     public function callback($provider)
     {
         try{
-            if($provider !== 'twitter'){
+            if($provider == 'twitter'){
                 $getInfo = Socialite::driver($provider)->user();
             }else{
                 $getInfo = Socialite::driver($provider)->stateless()->user();
