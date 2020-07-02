8c23b0421b98ac782e78a098547964dc5a3a7219
unknown <erojoseph94@gmail.com>
2020-07-02 19:32:56 +0100

    Admin fetch all a particular user testimonials

--- routes/api.php
+++ routes/api.php
@@ -54,7 +54,7 @@ Route::group(['namespace' => 'Api', 'middleware' => 'auth:api',  'prefix' => 'v1
     Route::get('completed-requests', 'AdminController@index');
 
    Route::delete('testimonials/delete/{id}','TestimonialController@deleteTestimonial');
-   Route::delete('testimonials','TestimonialController@index');
+   Route::delete('testimonials/{id}','TestimonialController@index');
 
 
     // Commented out by Eromosele
