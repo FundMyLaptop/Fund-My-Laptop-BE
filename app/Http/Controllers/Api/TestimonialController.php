4789807da1e533750184d0c40a3e4de10cb75137
unknown <erojoseph94@gmail.com>
2020-07-03 13:48:00 +0100

    fixes task 39 and 40

--- app/Http/Controllers/Api/TestimonialController.php
+++ app/Http/Controllers/Api/TestimonialController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Validacotor;
+use Validator;
 use App\Testimonial as Testimonial;
 use Illuminate\Support\Facades\Auth;
 
