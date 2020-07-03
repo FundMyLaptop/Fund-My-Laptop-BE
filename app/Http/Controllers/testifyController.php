a9900dd5899842c8f923200853055af125a8ea03
eMiracle <miraclechibuzo@gmail.com>
2020-07-03 12:47:49 +0100

    Task 38 debugged

--- app/Http/Controllers/testifyController.php
+++ app/Http/Controllers/testifyController.php
@@ -4,21 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use DB;
 
 class testifyController extends Controller
 {
-    public function delete_testimonial (Request $request)
+    public function delete(Request $request)
     {      
-        return $request;
+        //return $request;
         //validate post request as sent from user
         //testimonial_id is to be sent to this controller
-        $this->validate($request, [
-            'testimonial_id' => 'required'
-        ]);
-
-        $testimonial_id = $request()->testimonial_id;
-        
+        $testimonial_id = request()->testimonial_id;
+        //return $testimonial_id;
         //Check if user is authenticated
         if (Auth::check()) {
             $user_id = auth()->user()->id;
@@ -31,7 +28,7 @@ class testifyController extends Controller
             }
                 } else {
                     return response()->json([
-                        'message' => 'testimonial could not be fetched'
+                        'message' => 'testimonial could not be deleted'
                     ], 404);
                 }
             }
