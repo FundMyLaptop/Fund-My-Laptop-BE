1a29cd6cd8a56978e40ec58bf2fac96ead21b670
Olayemi Olatunji <38155220+Olat-nji@users.noreply.github.com>
2020-07-08 14:27:13 -0700

    Add files via upload

---
+++ app/Blog.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    protected $table = 'blogs';
+    
+    public $timestamps = True;
+    protected $primaryKey = 'id';
+  
+
+   
+}
