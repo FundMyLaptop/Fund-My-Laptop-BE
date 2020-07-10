f370d744c7cab2e6cd27f7f1bae7a2897e38da9a
Olayemi Olatunji <38155220+Olat-nji@users.noreply.github.com>
2020-07-10 16:36:22 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::get('benefit', 'PagesController@benefit');
 Route::get('partners', 'PagesController@partners');
 Route::get('how-it-works', 'PagesController@howItWorks');
 Route::get('milestones', 'PagesController@mileStones');
-Route::get('blog/{title}', 'PagesController@blogRead');
+Route::get('blog/{id}', 'PagesController@blogRead');
 Route::get('blog', 'PagesController@blog')->name('blog');
 Route::get('error404Page', 'PagesController@error404Page');
 Route::get('error500Page', 'PagesController@error500Page');
