ac816d46672869eee6d444beabe1080db338e0fa
boluakins <boluakins@gmail.com>
2020-07-16 22:57:04 +0100

    bug fixes

--- resources/views/includes/auth-header.blade.php
+++ resources/views/includes/auth-header.blade.php
@@ -29,8 +29,8 @@
                     <a class="nav-item nav-link px-4"><img src="{{asset('img/user-photo.png')}}" class="menu-icon" href="#" /></a>
                     <a class="nav-item nav-link pr-0 align-self-center" href="#">
                         <div class="position-relative">
-                            <img src="{{asset('img/notification-dot.svg'}}" alt="new notifications" class="position-absolute notification-dot" />
-                            <img src="{{asset('img/notification.svg'}}" class="notification-icon" alt="notification icon" />
+                            <img src="{{asset('img/notification-dot.svg')}}" alt="new notifications" class="position-absolute notification-dot" />
+                            <img src="{{asset('img/notification.svg')}}" class="notification-icon" alt="notification icon" />
                         </div>
                     </a>
                 </div>
