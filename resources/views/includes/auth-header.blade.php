e9d72529b80c776396b7512e4b9793e751bdd201
ajadi.paul <ajadi.paul@e4email.net>
2020-07-17 10:27:54 +0100

    conflicts

--- resources/views/includes/auth-header.blade.php
+++ resources/views/includes/auth-header.blade.php
@@ -1,3 +1,4 @@
+
 <!--Header-->
 <div class="nav-container container-fluid shadow-sm">
     <nav class="navbar nav-top navbar-expand-md navbar-light container-md px-0">
@@ -7,7 +8,7 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
+​
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto align-items-center">
                 <li class="nav-item">
@@ -27,11 +28,27 @@
                     <a class="nav-link px-4 pl-4 font-weight-bold" href="{{url('investee-dashboard')}}">Investee Dashboard</a>
                 </li>
                 <a href="{{url('investee-dashboard')}}"><button class="btn btn-fml-secondary align-self-center nav-item view-dashboard-btn" >  View Dashboard</button></a>
-                   
+​
+​
+                <div class="dropdown pl-4">
+                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="{{asset('img/user-photo.png')}}" class="menu-icon" href="#"/>
+                    </a>
                 
-
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a href="{{ url('update-profile') }}"> <button class="dropdown-item btn-fml-secondary">Update profile</button></a>
+                        <a  href="{{ route('logout') }}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            <button class="dropdown-item btn-fml-secondary">Log out</button>
+                        </a>
+​
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </div>
+​
                 <div class="navbar-nav flex-row toggle-menu">
-                    <a class="nav-item nav-link px-4" href="{{ url('update-profile') }}"><img src="{{asset('img/user-photo.png')}}" class="menu-icon"  /></a>
                     <a class="nav-item nav-link pr-0 align-self-center" href="#">
                         <div class="position-relative">
                             <img src="{{asset('img/notification-dot.svg') }}" alt="new notifications" class="position-absolute notification-dot" />
@@ -39,31 +56,6 @@
                         </div>
                     </a>
                 </div>
-
-                <li hidden class="nav-item">
-                    <a class="nav-link pl-md-4 font-weight-bold" href="{{  url('logout')  }}">
-                        <button class="btn btn-fml-secondary px-4">
-                          Log Out?
-                        </button>
-                    </a>
-                </li> 
-
-
-
-                <li class="nav-item">
-                    <a class="nav-link pl-md-4 font-weight-bold" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                        
-                        <button class="btn btn-fml-secondary px-4">
-                            {{ __('Logout') }}
-                        </button>
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </li>
             </ul>
         </div>
     </nav>
