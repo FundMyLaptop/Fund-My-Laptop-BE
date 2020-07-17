3d48dad9aae0e585ef02c378a385ab786ba87193
Paul AJadi <ajadi.ololade@gmail.com>
2020-07-17 08:12:10 +0100

    Broken links fixed (#256)
    * Fixed #182
    
    * fixed broken links
    
    * fixed #197
    
    * fixed #197
    
    * fixed broken links
    
    Co-authored-by: ajadi.paul <ajadi.paul@e4email.net>
    Co-authored-by: onifade boluwatife <48095055+onifs10@users.noreply.github.com>

--- resources/views/includes/auth-header.blade.php
+++ resources/views/includes/auth-header.blade.php
@@ -1,7 +1,7 @@
 <!--Header-->
 <div class="nav-container container-fluid shadow-sm">
     <nav class="navbar nav-top navbar-expand-md navbar-light container-md px-0">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="{{ url('/') }}">
             <img src="{{asset('img/layer1.png')}}" />
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,31 +17,53 @@
                     <a class="nav-link px-4 font-weight-bold" href="{{ url('borrow') }}">Borrow</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link px-4 font-weight-bold" href="{{ url('/#about') }}">About</a>
+                    <a class="nav-link px-4 font-weight-bold" href="{{ url('about') }}">About</a>
                 </li>
                 
-                <li class="nav-item">
-                <button class="btn btn-fml-secondary align-self-center nav-item view-dashboard-btn" href="{{url('investor-dashboard')}}">View Dashboard</button>
-                   
+                <li hidden class="nav-item">
+                    <a class="nav-link px-4 pl-4 font-weight-bold" href="{{url('investor-dashboard')}}">Investor Dashboard</a>
                 </li>
+                <li hidden class="nav-item">
+                    <a class="nav-link px-4 pl-4 font-weight-bold" href="{{url('investee-dashboard')}}">Investee Dashboard</a>
+                </li>
+                <a href="{{url('investee-dashboard')}}"><button class="btn btn-fml-secondary align-self-center nav-item view-dashboard-btn" >  View Dashboard</button></a>
+                   
+                
 
                 <div class="navbar-nav flex-row toggle-menu">
-                    <a class="nav-item nav-link px-4"><img src="{{asset('img/user-photo.png')}}" class="menu-icon" href="#" /></a>
+                    <a class="nav-item nav-link px-4" href="{{ url('update-profile') }}"><img src="{{asset('img/user-photo.png')}}" class="menu-icon"  /></a>
                     <a class="nav-item nav-link pr-0 align-self-center" href="#">
                         <div class="position-relative">
-                            <img src="{{asset('img/notification-dot.svg'}}" alt="new notifications" class="position-absolute notification-dot" />
-                            <img src="{{asset('img/notification.svg'}}" class="notification-icon" alt="notification icon" />
+                            <img src="{{asset('img/notification-dot.svg') }}" alt="new notifications" class="position-absolute notification-dot" />
+                            <img src="{{asset('img/notification.svg') }}" class="notification-icon" alt="notification icon" />
                         </div>
                     </a>
                 </div>
 
-                <li class="nav-item">
+                <li hidden class="nav-item">
                     <a class="nav-link pl-md-4 font-weight-bold" href="{{  url('logout')  }}">
                         <button class="btn btn-fml-secondary px-4">
                           Log Out?
                         </button>
                     </a>
                 </li> 
+
+
+
+                <li class="nav-item">
+                    <a class="nav-link pl-md-4 font-weight-bold" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                        
+                        <button class="btn btn-fml-secondary px-4">
+                            {{ __('Logout') }}
+                        </button>
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
             </ul>
         </div>
     </nav>
