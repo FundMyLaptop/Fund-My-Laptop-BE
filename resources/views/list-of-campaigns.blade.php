abbb5a2d3521288cba919a3defaf114aa67a18e3
aveirahabeeb <aveirahabeeb@gmail.om>
2020-07-18 09:08:02 +0100

    Campaign listing page feature

--- resources/views/list-of-campaigns.blade.php
+++ resources/views/list-of-campaigns.blade.php
@@ -14,235 +14,81 @@
     </div>
 
     <h2 class="topCampaigns">Top Campaigns</h2>
+
     <div class="row">
-      <div class="box">
-        <img class="img" src="../img/pic1.png" />
+      @forelse($topcampaigns as $topcampaigns)
+      <div class="box ">
+        <img class="img" src="{{ $topcampaigns->photoURL }}" />
         <div class="boxContent">
-          <h2 class="name">Amaka Onidoe</h2>
-          <p class="reason">Laptop Purchase</p>
-          <p class="description">I need a laptop to complete my final year school project...</p>
+          <h2 class="name">{{ $topcampaigns->user->firstName}} {{$topcampaigns->user->lastName }}</h2>
+          <p class="reason">{{ $topcampaigns->title }}</p>
+          <p class="description">{{ $topcampaigns->description }}</p>
 
           <!--Progress Bar-->
           <div class="meter">
-            <div class="span" style="width: 25%"></div>
+            <div class="span" style="width: {{ $topcampaigns->transaction->sum('amount') / ($topcampaigns->amount) * (100) }}%; max-width: 100%"></div>
           </div>
-
 
 
           <div class="d-flex justify-content-between amount">
-            <h3>N10,000</h3>
-            <h3>10%</h3>
+            <h3> &#x20a6;{{ number_format($topcampaigns->transaction->sum('amount')) ?? 0 }}</h3>
+            <h3>{{ round($topcampaigns->transaction->sum('amount') / ($topcampaigns->amount) * (100), 0) }}%</h3>
           </div>
-          <p class="raisedAmount">Raised of N100,000</p>
+          <p class="raisedAmount">Raised of &#x20a6;{{ number_format($topcampaigns->amount) }}</p>
         </div>
-        <button class="detailsButton">View Details <i class="fas fa-arrow-right"></i></button>
+        <button onclick="location.href='{{ url('/campaign/'.$topcampaigns->id) }}'" class="detailsButton">View Details <i class="fas fa-arrow-right"></i></button>
       </div>
-      <div class="box">
-        <img class="img" src="../img/pic2.png" />
-        <div class="boxContent">
-          <h2 class="name">Richard Popsabey</h2>
-          <p class="reason">Laptop Purchase</p>
-          <p class="description">I need a laptop to complete my final year school project...</p>
-
-
-
-          <!--Progress Bar-->
-          <div class="meter">
-            <div class="span" style="width: 25%"></div>
-          </div>
-
-
-
-          <div class="d-flex justify-content-between amount">
-            <h3>N90,000</h3>
-            <h3>90%</h3>
-          </div>
-          <p class="raisedAmount">Raised of N100,000</p>
-        </div>
-        <button class="detailsButton">View Details <i class="fas fa-arrow-right"></i></button>
+      @empty
+      <div class="col-sm-12 col-md-6 col-lg-6 offset-md-3 offset-lg-3">
+          <h3 class="mt-4 text-center"> {{ "No Top Campaigns" }} </h3>
       </div>
-      <div class="box hide">
-        <img class="img" src="../img/pic3.png" />
-        <div class="boxContent">
-          <h2 class="name">Kunle Doe</h2>
-          <p class="reason">Laptop Purchase</p>
-          <p class="description">I need a laptop to complete my final year school project...</p>
+      @endforelse
 
-
-
-          <!--Progress Bar-->
-          <div class="meter">
-            <div class="span" style="width: 0%"></div>
-          </div>
-
-
-
-          <div class="d-flex justify-content-between amount">
-            <h3>N0</h3>
-            <h3>10%</h3>
-          </div>
-          <p class="raisedAmount">Raised of N100,000</p>
-        </div>
-        <button class="detailsButton">View Details <i class="fas fa-arrow-right"></i></button>
-      </div>
     </div>
     <div class="d-flex justify-content-end">
-      <button class="btn btn-white see-more" style="color:#FB3F5C">See more ></button>
+      <button  onclick="location.href='{{ url('/campaign-grossing') }}'" class="btn btn-white see-more" style="color:#FB3F5C">See more ></button>
     </div>
+    
 </section>
+   
 
-
+  
   <section class="container">
     <h2 class="topCampaigns">Featured Campaigns</h2>
+    
     <div class="row">
-      <div class="box">
-        <img class="img" src="../img/pic4.png" />
+      @forelse($featuredCampaigns as $featuredCampaign)
+      <div class="box ">
+        <img class="img" src="{{ $featuredCampaign->photoURL }}" />
         <div class="boxContent">
-          <h2 class="name">Jane Doe</h2>
-          <p class="reason">Laptop Purchase</p>
-          <p class="description">I need a laptop to complete my final year school project...</p>
-
-
+          <h2 class="name">{{ $featuredCampaign->user->firstName}} {{$featuredCampaign->user->lastName }}</h2>
+          <p class="reason">{{ $featuredCampaign->title }}</p>
+          <p class="description">{{ $featuredCampaign->description }}</p>
 
           <!--Progress Bar-->
           <div class="meter">
-            <div class="span" style="width: 25%"></div>
+            <div class="span" style="width: {{ $topcampaigns->transaction->sum('amount') / ($topcampaigns->amount) * (100) }}%; max-width: 100%"></div>
           </div>
-
-
 
           <div class="d-flex justify-content-between amount">
-            <h3>N10,000</h3>
-            <h3>10%</h3>
+            <h3>&#x20a6;{{ number_format($topcampaigns->transaction->sum('amount')) ?? 0 }}</h3>
+            <h3>{{ round($topcampaigns->transaction->sum('amount') / ($topcampaigns->amount) * (100), 0) }}%</h3>
           </div>
-          <p class="raisedAmount">Raised of N100,000</p>
+          <p class="raisedAmount">Raised of &#x20a6;{{ number_format($featuredCampaign->amount) }}</p>
         </div>
-        <button class="detailsButton">View Details <i class="fas fa-arrow-right"></i></button>
+        <button onclick="location.href='{{ url('/campaign/'.$featuredCampaign->id) }}'" class="detailsButton">View Details <i class="fas fa-arrow-right"></i></button>
       </div>
-      <div class="box">
-        <img class="img" src="../img/pic5.png" />
-        <div class="boxContent">
-          <h2 class="name">Julia Orji</h2>
-          <p class="reason">Laptop Purchase</p>
-          <p class="description">I need a laptop to complete my final year school project...</p>
 
-
-
-          <!--Progress Bar-->
-          <div class="meter">
-            <div class="span" style="width: 90%"></div>
+          @empty
+          <div class="col-sm-12 col-md-6 col-lg-6 offset-md-3 offset-lg-3">
+              <h3 class="mt-4 text-center"> {{ "No Featured Campaigns" }} </h3>
           </div>
-
-
-
-          <div class="d-flex justify-content-between amount">
-            <h3>N90,000</h3>
-            <h3>90%</h3>
-          </div>
-          <p class="raisedAmount">Raised of N100,000</p>
-        </div>
-        <button class="detailsButton">View Details <i class="fas fa-arrow-right"></i></button>
-      </div>
-      <div class="box">
-        <img class="img" src="../img/pic6.png" />
-        <div class="boxContent">
-          <h2 class="name">Kunle Doe</h2>
-          <p class="reason">Laptop Purchase</p>
-          <p class="description">I need a laptop to complete my final year school project...</p>
-
-
-
-          <!--Progress Bar-->
-          <div class="meter">
-            <div class="span" style="width: 0%"></div>
-          </div>
-
-
-
-          <div class="d-flex justify-content-between amount">
-            <h3>N0</h3>
-            <h3>10%</h3>
-          </div>
-          <p class="raisedAmount">Raised of N100,000</p>
-        </div>
-        <button class="detailsButton">View Details <i class="fas fa-arrow-right"></i></button>
-      </div>
-      <div class="box">
-        <img class="img" src="../img/pic1.png" />
-        <div class="boxContent">
-          <h2 class="name">Fidel Komolafe</h2>
-          <p class="reason">Laptop Purchase</p>
-          <p class="description">I need a laptop to complete my final year school project...</p>
-
-
-
-          <!--Progress Bar-->
-          <div class="meter">
-            <div class="span" style="width: 0%"></div>
-          </div>
-
-
-
-          <div class="d-flex justify-content-between amount">
-            <h3>N0</h3>
-            <h3>10%</h3>
-          </div>
-          <p class="raisedAmount">Raised of N100,000</p>
-        </div>
-        <button class="detailsButton">View Details <i class="fas fa-arrow-right"></i></button>
-      </div>
-      <div class="box hide">
-        <img class="img" src="../img/pic2.png" />
-        <div class="boxContent">
-          <h2 class="name">Genesis Anosike</h2>
-          <p class="reason">Laptop Purchase</p>
-          <p class="description">I need a laptop to complete my final year school project...</p>
-
-
-
-          <!--Progress Bar-->
-          <div class="meter">
-            <div class="span" style="width: 10%"></div>
-          </div>
-
-
-
-          <div class="d-flex justify-content-between amount">
-            <h3>N10,000</h3>
-            <h3>10%</h3>
-          </div>
-          <p class="raisedAmount">Raised of N100,000</p>
-        </div>
-        <button class="detailsButton">View Details <i class="fas fa-arrow-right"></i></button>
-      </div>
-      <div class="box hide">
-        <img class="img" src="../img/pic3.png" />
-        <div class="boxContent">
-          <h2 class="name">Richard Popsabey</h2>
-          <p class="reason">Laptop Purchase</p>
-          <p class="description">I need a laptop to complete my final year school project...</p>
-
-
-
-          <!--Progress Bar-->
-          <div class="meter">
-            <div class="span" style="width: 0%"></div>
-          </div>
-
-
-
-          <div class="d-flex justify-content-between amount">
-            <h3>N4,000</h3>
-            <h3>10%</h3>
-          </div>
-          <p class="raisedAmount">Raised of N100,000</p>
-        </div>
-        <button class="detailsButton">View Details <i class="fas fa-arrow-right"></i></button>
-      </div>
+          @endforelse
     </div>
     <div class="d-flex justify-content-end">
-      <button class="btn btn-white see-more" style="color:#FB3F5C">See more ></button>
+      <button onclick="location.href='{{ url('/featured-request') }}'"  class="btn btn-white see-more" style="color:#FB3F5C">See more ></button>
     </div>
+    
   </section>
 @endsection
 
