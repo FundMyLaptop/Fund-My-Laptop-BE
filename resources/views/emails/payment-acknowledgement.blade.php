ebacb1f9d91891b358c07c1a77d2e80f003ff673
Chidi Okpala <63919087+ChidiOkpala@users.noreply.github.com>
2020-07-18 11:11:27 +0100

    addition of investemnt email and payment-acknowledgement blade pages

---
+++ resources/views/emails/payment-acknowledgement.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap" rel="stylesheet" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title></title>
+  </head>
+  <body>
+	<div class="" style="font-family:DM Sans">
+		<div class="container-fluid bg-light">
+			<h6 class="text-center py-4" style="color:#FB3F5C">Fund-My-Laptop</h6>
+		</div>
+		<div class="container bg-white">
+			<div class="w-50">
+				<h2 class="pt-3">
+					Hi Jeramy Ankunding
+				</h2>
+				<p>Your campaign <a href="" style="color:#FB3F5C">http://localhost:800/campaign/50</a> has been funded with N50,000</p>
+				<p>Congratulations!</p>
+				<p>Regards,<br>Fund-My-Laptop</p>
+				<br><br>
+			</div>
+		</div>
+		<div class="container-fluid bg-light">
+			<h6 class="text-center py-4" style="color:#FB3F5C">&copy; 2020 Fund-My-Laptop. All rights reserved</h6>
+		</div>
+	</div>
+
+    
+    
+  </body>
+</html>
